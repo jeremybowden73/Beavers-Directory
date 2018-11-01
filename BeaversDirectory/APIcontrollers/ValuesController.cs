ae1390d308899a521611625acdde15dee7de13f6
Jeremy Bowden <jeremybowden@msn.com>
2018-11-01 22:05:37 +0000

    Refactor "for" loop to "foreach" in Get; revise Names.

--- BeaversDirectory/APIcontrollers/ValuesController.cs
+++ BeaversDirectory/APIcontrollers/ValuesController.cs
@@ -32,61 +32,86 @@ namespace BeaversDirectory.APIcontrollers
             [FromQuery]string Town,
             [FromQuery]string Lodge,
             [FromQuery]string Leader)
-        {
-            var allBeavers = _beaversRepository.AllBeavers().OrderBy(b => b.Id).ToList();
-
-            if (FirstName != null)
             {
-                allBeavers = allBeavers.FindAll(x => x.FirstName == FirstName);
-            }
-          
-            if (LastName != null)
-            {
-                allBeavers = allBeavers.FindAll(x => x.LastName == LastName);
-            }
+                var allBeavers = _beaversRepository.AllBeavers().OrderBy(b => b.Id).ToList();
 
-            if (Town != null)
-            {
-                allBeavers = allBeavers.FindAll(x => x.Town == Town);
-            }
-
-            if (Lodge != null)
-            {
-                allBeavers = allBeavers.FindAll(x => x.Lodge == Lodge);
-            }
-
-            if (Leader != null && Leader.ToLower() == "true")
-            {
-                allBeavers = allBeavers.FindAll(x => x.IsLodgeLeader == true);
-            }
-
-            // return a status code 404 if there are no objects to return to the API consumer
-            if (allBeavers.Count == 0)
-            {
-                return NotFound();
-            }
-
-            // BeaverLiteAPI is a simplified version of Beaver that is suitable for serving to
-            // the API consumer (it does not contain DOB or Parent information)
-            List<BeaverLiteAPI> beavers = new List<BeaverLiteAPI>();
-
-            // for each Beaver in allBeavers, create a BeaverLiteAPI object (by populating
-            // it with data from the Beaver object) then add it to the List "beavers" 
-            for (int i = 0; i < allBeavers.Count; i++)
-            {
-                BeaverLiteAPI nextBeaverToAdd = new BeaverLiteAPI
+                if (FirstName != null)
                 {
-                    FirstName = allBeavers[i].FirstName,
-                    LastName = allBeavers[i].LastName,
-                    Town = allBeavers[i].Town,
-                    Lodge = allBeavers[i].Lodge,
-                    IsLodgeLeader = allBeavers[i].IsLodgeLeader
+                    allBeavers = allBeavers.FindAll(x => x.FirstName == FirstName);
+                }
+          
+                if (LastName != null)
+                {
+                    allBeavers = allBeavers.FindAll(x => x.LastName == LastName);
+                }
+
+                if (Town != null)
+                {
+                    allBeavers = allBeavers.FindAll(x => x.Town == Town);
+                }
+
+                if (Lodge != null)
+                {
+                    allBeavers = allBeavers.FindAll(x => x.Lodge == Lodge);
+                }
+
+                if (Leader != null && Leader.ToLower() == "true")
+                {
+                    allBeavers = allBeavers.FindAll(x => x.IsLodgeLeader == true);
+                }
+
+                // return a status code 404 if there are no objects to return to the API consumer
+                if (allBeavers.Count == 0)
+                {
+                    return NotFound();
+                }
+
+                // BeaverLiteAPI is a simplified version of Beaver that is suitable for serving to
+                // the API consumer (it does not contain DOB or Parent information)
+                List<BeaverLiteAPI> beavers = new List<BeaverLiteAPI>();
+
+                foreach (Beaver element in allBeavers)
+                {
+                    BeaverLiteAPI nextBeaverToAdd = new BeaverLiteAPI
+                    {
+                        FirstName = element.FirstName,
+                        LastName = element.LastName,
+                        Town = element.Town,
+                        Lodge = element.Lodge,
+                        IsLodgeLeader = element.IsLodgeLeader
+                    };
+
+                    beavers.Add(nextBeaverToAdd);
+                }
+
+                return beavers;
+            }
+
+
+        // GET: api/values/names
+        [HttpGet("names")]
+        public ActionResult<List<BeaverName>> Names()
+        {
+            var allBeavers = _beaversRepository.AllBeavers().OrderBy(b => b.LastName).ToList();
+
+            List<BeaverName> beaverNames = new List<BeaverName>();
+
+            // for each object in allBeavers, add a new BeaverName object to beaverNames,
+            // which contains the Id and the concatanated FirstName and LastName properties
+            foreach (Beaver element in allBeavers)
+            {
+                BeaverName nextBeaverName = new BeaverName
+                {
+                    Name = element.LastName + " " + element.FirstName
                 };
 
-                beavers.Add(nextBeaverToAdd);
+                beaverNames.Add(nextBeaverName);
             }
 
-            return beavers;
+
+
+
+            return beaverNames;
         }
     }
 }
